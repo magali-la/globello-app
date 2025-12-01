34e1d0817610e31965e11ef31bd86d7bfd19fddb
Mag <166319982+magali-la@users.noreply.github.com>
2025-12-01 00:09:27 -0500

    feat(api): add api script file to fetch data with try catch method for errors

---
+++ src/scripts/apiService.js
@@ -0,0 +1,20 @@
+// fetch API data from REST Countries API
+export async function fetchAllCountries() {
+    try {
+        console.log(`Attempting to retrieve API country data`);
+        const response = await fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,borders,nativeName,flag,languages');
+
+        // use fetch methods to identify errors
+        if (!response.ok) {
+            throw new Error('API failed to retrieve all countries data')
+        }
+
+        const countryData = await response.json();
+        console.log(`API country data retrieved successfully`);
+
+        // return the data to import elsewhere
+        return countryData;
+    } catch (error) {
+        console.error(`Error found while fetching all countries`, error);
+    }
+};
