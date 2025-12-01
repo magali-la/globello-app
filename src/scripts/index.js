8e800ca6fb822ff602060557d43d80836ffbfc57
Mag <166319982+magali-la@users.noreply.github.com>
2025-12-01 07:37:25 -0500

    feat(search): add search event listener, move displayCountries function to global scope for reuse, remove template to build language label in DOM

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,63 +2,86 @@ import { fetchAllCountries } from "./apiService.js";
 
 // define global variables for DOM elements
 const countryCardGrid = document.querySelector("#countryCardGrid");
+const searchInput = document.querySelector("#countrySearch");
+let countryData = [];
+
+// run the fetch all country data and store in a variable to display in the grid
+async function displayAllCountries(countryData) {
+    // define the country card template clone
+    const templateCountryCard = document.querySelector(".templateCountryCard");
+
+    // clear the grid
+    countryCardGrid.innerHTML = '';
+    countryData.forEach(country => {
+        // clone a template 
+        let clonedCountryCard = templateCountryCard.cloneNode(true);
+        clonedCountryCard.classList.remove('hidden');
+
+        // add flag to card
+        let flagImg = clonedCountryCard.querySelector("img");
+        flagImg.src = `${country.flags.svg}`;
+        flagImg.alt = `${country.flags.alt}`;
+
+        // add name to card
+        let countryName = clonedCountryCard.querySelector(".countryName");
+        let officialName = clonedCountryCard.querySelector(".officialName");
+        countryName.innerText = `${country.name.common}`;
+
+        // logic to display official name if it's different from countryName
+        if (country.name.common !== country.name.official){
+            officialName.innerText = `${country.name.official}`;
+        }
+
+        // add region and capital to card
+        let regionName = clonedCountryCard.querySelector(".regionName");
+        let countryCapital = clonedCountryCard.querySelector(".countryCapital");
+        regionName.innerText = `${country.region}`;
+        countryCapital.innerText = `${country.capital}`;
+
+        // add languages
+        const langContainer = clonedCountryCard.querySelector(".languagesContainer");
+        // clear what's in the lang container to accommodate search results
+        langContainer.innerHTML = '';
+        
+
+        Object.values(country.languages).forEach(language => {
+            // create the element in the DOM
+            const langLabel = document.createElement('span');
+            langLabel.classList.add('templateLangLabel', 'px-2', 'py-1', 'rounded-lg', 'bg-school', 'text-base');
+            langLabel.innerText = language;
+            // append it to the container
+            langContainer.append(langLabel);
+        });
+
+        // append the clonedCard to the grid
+        countryCardGrid.append(clonedCountryCard);
+    });
+
+
+}
 
 // Event listener to display country data in grid
-window.addEventListener("load", () => {
-    // run the fetch all country data and store in a variable to display in the grid
-    async function displayAllCountries() {
-        const countryData = await fetchAllCountries();
-        console.log(`API countries data unwrapped in index.js`);
+window.addEventListener("load", async () => {
+    countryData = await fetchAllCountries();
+    console.log(`API countries data unwrapped in index.js`);
+    displayAllCountries(countryData);
+});
 
-        // define the country card template clone
-        const templateCountryCard = document.querySelector(".templateCountryCard");
 
-        countryData.forEach(country => {
-            // clone a template 
-            let clonedCountryCard = templateCountryCard.cloneNode(true);
-            clonedCountryCard.classList.remove('hidden');
-
-            // add flag to card
-            let flagImg = clonedCountryCard.querySelector("img");
-            flagImg.src = `${country.flags.svg}`;
-            flagImg.alt = `${country.flags.alt}`;
-
-            // add name to card
-            let countryName = clonedCountryCard.querySelector(".countryName");
-            let officialName = clonedCountryCard.querySelector(".officialName");
-            countryName.innerText = `${country.name.common}`;
-
-            // logic to display official name if it's different from countryName
-            if (country.name.common !== country.name.official){
-                officialName.innerText = `${country.name.official}`;
-            }
-
-            // add region and capital to card
-            let regionName = clonedCountryCard.querySelector(".regionName");
-            let countryCapital = clonedCountryCard.querySelector(".countryCapital");
-            regionName.innerText = `${country.region}`;
-            countryCapital.innerText = `${country.capital}`;
-
-            // add languages
-            const langContainer = clonedCountryCard.querySelector(".languagesContainer");
-            const langLabel = clonedCountryCard.querySelector(".templateLangLabel");
-            langLabel.innerText = ''
-            
-
-            Object.values(country.languages).forEach(language => {
-                const clonedLangLabel = langLabel.cloneNode(true);
-                clonedLangLabel.classList.remove('hidden');
-                clonedLangLabel.innerText = language;
-                // append it to the container
-                langContainer.append(clonedLangLabel);
-            });
-    
-            // append the clonedCard to the grid
-            countryCardGrid.append(clonedCountryCard);
-        });
-    
-
+// search bar functionality
+searchInput.addEventListener("input", (event) => {
+    if (countryData.length === 0){
+        return;
     }
+    
+    // set the user's input to lowercase
+    let userSearch = event.target.value.toLowerCase();
 
-    displayAllCountries();
+    // filter the items into a new array
+    const searchFilter = countryData.filter(country => 
+        country.name.common.toLowerCase().includes(userSearch)
+    );
+
+    // run display countries function with filtered items
+    displayAllCountries(searchFilter);
 });
