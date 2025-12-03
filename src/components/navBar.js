83da2d06845f32184559eccb345e73f4d544ad68
Mag <166319982+magali-la@users.noreply.github.com>
2025-12-03 18:47:31 -0500

    feat(dark mode): add dark mode setting to local storage to persist across pages and revisit

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -28,18 +28,38 @@ window.addEventListener("load", () => {
     // append the new instance on page load
     navContainer.appendChild(navContent);
 
+    // dark mode logic - define variables for toggle button and the body element where tailwind dark class toggles
     const toggleButton = document.querySelector(".toggle-btn");
+    const bodyEl = document.body;
+
+    // create variable for whether dark mode is on or not with default false, which is regular mode if a user never clicks the button while using the site
+    let onDarkMode = localStorage.getItem("darkMode") || false;
+
+    // conditional to set the dark mode 
+    if (onDarkMode === 'true') {
+        console.log('Dark Mode retrieved via user preference');
+        // then switch to dark mode
+        bodyEl.classList.add("dark");
+        toggleButton.innerHTML = `<span class="mr-4"><i class="bi bi-sun"></i></span>Light Mode`;
+    } else {
+        console.log('Light modee retrieved via user preference')
+    }
+
 
     toggleButton.addEventListener("click", () => {
         console.log("toggle button clicked");
-        const bodyEl = document.body;
         bodyEl.classList.toggle("dark");
 
         // conditional for inner text change
         if (bodyEl.classList.contains("dark")) {
-            toggleButton.innerHTML = `<span class="mr-4"><i class="bi bi-sun"></i></span>Light Mode`
+            toggleButton.innerHTML = `<span class="mr-4"><i class="bi bi-sun"></i></span>Light Mode`;
+
+            // store in local storage
+            localStorage.setItem('darkMode', 'true');
         } else {
-            toggleButton.innerHTML = `<span class="mr-4"><i class="bi bi-moon-stars"></i></span>Dark Mode`
+            toggleButton.innerHTML = `<span class="mr-4"><i class="bi bi-moon-stars"></i></span>Dark Mode`;
+
+            localStorage.setItem('darkMode', 'false');
         }
     });
 });
